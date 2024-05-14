e78884209a98fe6f58e2d05f4492c6de39b7420b
Chaehwan Lim <chlim428@gmail.com>
2024-05-14 13:30:04 +0900

    Initial spring boot setting

---
+++ src/main/java/org/causwengteam13/issuetrackerserver/IssueTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package org.causwengteam13.issuetrackerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/causwengteam13/issuetrackerserver/IssueTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.causwengteam13.issuetrackerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
